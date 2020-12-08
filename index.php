8145fac4ec6587f80f5e5b35471415f16a46cafa
abhi4469 <abhijeetdev4469@gmail.com>
2020-12-08 05:29:35 +0000

    S2I Practice

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 <html>
 <body>
 
-<h1>This is openshift 3.11:: </h1>
+<h1>This is openshift 4.0 </h1>
 
 <?php
 echo "Hello coming from php git World!s2i";
-echo "This is s2i project of openshift on 4.4";
 ?>
 
 </body>
